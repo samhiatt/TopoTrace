6cf9274dbc631cb8c078c04388d3774d845b5b99
Sam Hiatt <sam.hiatt@gmail.com>
2015-10-19 22:58:25 -0700

    profile db queries and elevProfile lookup

--- find_station_neighbors.py
+++ find_station_neighbors.py
@@ -1,39 +1,47 @@
 __author__ = 'Sam Hiatt'
-
 from pymongo import MongoClient
 import re
 import numpy as np
+from datetime import datetime
 from pandas import DataFrame
 from GTOPO30 import getElevationProfile
 from LatLon import LatLon
 
+FEET_PER_METER = 3.28084
+ELEV_THRESHOLD = 200/FEET_PER_METER
+TOPO_THRESHOLD = 500/FEET_PER_METER
+DISTANCE_THRESHOLD = 15000
 
 client = MongoClient('localhost',27017)
-
 db = client['stationdata']
-
 stations=db['stations']
-
 stations.ensure_index([("loc","2dsphere")])
 
-
-for station in stations.find({
+stationsQuery = stations.find({
         "_id":re.compile('^KCASANFR')
-    }).limit(5):
+    }).limit(5)
+print "%i stations"%stationsQuery.count()
+for station in stationsQuery:
     # Get nearby stations
+    t0neighborQuery = datetime.now()
     neighborQuery = stations.find({
             "_id":{"$ne":station['_id']},
             "loc":{"$near":{"$geometry":station['loc'],
-               "$maxDistance":3000}
+               "$maxDistance":DISTANCE_THRESHOLD}
               }
         }).limit(5)
+    t0NeighborLoop = datetime.now()
+    dt = datetime.now() - t0neighborQuery
+    print("Neighbor query: %.6fs"%dt.total_seconds())
     if neighborQuery.count()==np.nan:
-        print "Station %s has no neighbors."%station['_id']
+        print "Station %s has no nearby stations."%station['_id']
     else:
-        print "%s's neighbors:"%station['_id']
+        print "%s's %i neighbors:"%(station['_id'],neighborQuery.count())
         c0 = station['loc']['coordinates']
         stationLoc = LatLon(c0[1],c0[0])
+        neighborCount = 0
         for neighbor in neighborQuery:
+            neighborCount+=1
             c1 = neighbor['loc']['coordinates']
             elevProfile = DataFrame(getElevationProfile(c0[0],c0[1],c1[0],c1[1]),
                                     columns=('lon','lat','elev'))
@@ -49,3 +57,4 @@ for station in stations.find({
             #  - check proximity to water
 
             print(neighbor['_id'], elevs.max(),relativePeakHeight,dist)
+    print("StationLoop: %i stations, %.6fs"%(neighborCount,(datetime.now()-t0NeighborLoop).total_seconds()))
