77f0bf0d82cfab9209cf88cdfbaacd87c7274c80
Sam Hiatt <sam.hiatt@weather.com>
2015-10-21 20:47:52 -0700

    Added get_stations script

---
+++ get_stations.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+import json
+
+client = MongoClient('localhost',27099)
+db = client['stationdata']
+stations=db['stations']
+
+stationsCursor = stations.find({
+                                    "updated":{"$exists":1}
+                                },{
+                                    "loc":1
+                                })
+print ("%i stations."%stationsCursor.count())
+res = []
+for station in stationsCursor:
+    res.append(station)
+
+with open("stations.json",'w') as outfile:
+    outfile.write(json.dumps(res))
+
+print "Done."
